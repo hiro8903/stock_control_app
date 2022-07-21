97c699ab463fe7f27fa29ba3f477d42f2740453c
Hiroki Araki <hiro8903@gmail.com>
2022-07-21 12:29:40 +0900

    Userにpasswordとpassword_confirmationカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_16_054401) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_032642) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
