8d7e04f6cb5f93225335784a38c9c52e32565acb
Hiroki Araki <hiro8903@gmail.com>
2022-07-22 07:15:00 +0900

    ログイン関連のbefore_actionを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @users =User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -23,12 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    
     if @user.update(user_params)
       log_in @user
       flash[:success] = '更新に成功しました。'
@@ -41,16 +41,33 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
     redirect_to root_path, status: :see_other # ステータスコード303 See Otherでrootパスにリダイレクトします。
   end
-  
-
-
-
   
   private
     def user_params
       params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to root_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      flash[:danger] = "権限がありません。"
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
