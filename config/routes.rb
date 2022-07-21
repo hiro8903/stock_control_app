49e9d0c483dfb6a8f710b60e6c3a1b8f13d11230
Hiroki Araki <hiro8903@gmail.com>
2022-07-21 19:19:49 +0900

    sessions_controller作成、routes.rbの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'sessions#new'
+
+  # ログイン機能
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
