efa955fb74d6429032f51d17ef2e1d7c9ff0b28e
Hiroki Araki <hiro8903@gmail.com>
2022-07-27 22:33:32 +0900

    UsersテーブルにDepartmentカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_27_034222) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_27_133111) do
   create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_034222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.integer "department_id"
+    t.index ["department_id"], name: "index_users_on_department_id"
   end
 
+  add_foreign_key "users", "departments"
 end
