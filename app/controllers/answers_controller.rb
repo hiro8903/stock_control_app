0a1893ba93c35236978227cc179a40c62dec3b60
Hiroki Araki <hiro8903@gmail.com>
2022-08-18 22:46:04 +0900

    Answer用のリンク、indexページ作成

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,6 @@
 class AnswersController < ApplicationController
   def index
+    @answers = Answer.all
   end
 
   def show
--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,8 @@
 module AnswersHelper
+
+# 納入量回答に対しての注文残量[kg]
+  def residual(answer)
+    answer.order.quantity - answer.quantity == 0 ? "-" : answer.order.quantity - answer.quantity 
+  end
+  
 end
