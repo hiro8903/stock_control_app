1aa797b734429b7bc0bf7d8b3701a39957a2168c
Hiroki Araki <hiro8903@gmail.com>
2022-08-19 20:01:05 +0900

    Answerの納入予定量を入力フォームで制限（ヘルパーメソッド・バリデーション・フォーム修正）

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,23 +1,16 @@
 module AnswersHelper
 
-# 納入量回答に対しての注文残量[kg]
+# 納入予定量回答の総量に対しての注文残量[kg]
   def residual(answer)
     answers = answer.order.answers
     answers_total_quantity = 0
     answers.each do |ans|
       answers_total_quantity =+ ans.quantity
     end
-    if answers_total_quantity != 0
-      (answer.order.quantity - answers_total_quantity) == 0.0 ? "-" : (answer.order.quantity - answers_total_quantity)
+    if answers_total_quantity == 0
+      answer.order.quantity
+    else
+      answer.order.quantity - answers_total_quantity
     end
   end
-  
-  # 納入量回答に対しての注文残量[kg]
-  # def residual(answer)
-  #   if answer.quantity.present?
-  #     answer.order.quantity - answer.quantity == 0 ? "-" : answer.order.quantity - answer.quantity 
-  #   else
-  #     return answer.order.quantity
-  #   end
-  # end
 end
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,4 @@
 class Answer < ApplicationRecord
   # include AnswersHelper
   belongs_to :order, class_name: 'Order',foreign_key: 'order_id'
-  # validate :quantity_orver
-
-  def quantity_orver
-    debugger
-    errors.add("発注量を超えています。") if quantity > Answer.order.quantity
-  end
 end
