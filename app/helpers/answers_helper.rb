4c7aa10c87885ddd010dbbc0d94cd2dbf7afc4af
Hiroki Araki <hiro8903@gmail.com>
2022-08-19 15:14:27 +0900

    Answerの新規登録画面・フォームに項目追加、ヘルパーに残量（発注量ー回答量）のメソッド作成

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -2,11 +2,22 @@ module AnswersHelper
 
 # 納入量回答に対しての注文残量[kg]
   def residual(answer)
-    if answer.quantity.present?
-      answer.order.quantity - answer.quantity == 0 ? "-" : answer.order.quantity - answer.quantity 
-    else
-      return answer.order.quantity
+    answers = answer.order.answers
+    answers_total_quantity = 0
+    answers.each do |ans|
+      answers_total_quantity =+ ans.quantity
+    end
+    if answers_total_quantity != 0
+      (answer.order.quantity - answers_total_quantity) == 0.0 ? "-" : (answer.order.quantity - answers_total_quantity)
     end
   end
   
+  # 納入量回答に対しての注文残量[kg]
+  # def residual(answer)
+  #   if answer.quantity.present?
+  #     answer.order.quantity - answer.quantity == 0 ? "-" : answer.order.quantity - answer.quantity 
+  #   else
+  #     return answer.order.quantity
+  #   end
+  # end
 end
