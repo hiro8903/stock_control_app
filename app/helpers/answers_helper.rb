a4ed6157008a7b2394bb809ed6bfe0c8905ade62
Hiroki Araki <hiro8903@gmail.com>
2022-08-19 20:46:53 +0900

    OrderのindexページにAnswerの登録・編集ボタンの表示、Oderヘルパーに注文残のメソッド追加

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,11 +1,11 @@
 module AnswersHelper
 
 # 納入予定量回答の総量に対しての注文残量[kg]
-  def residual(answer)
+  def answers_helper_residual(answer)
     answers = answer.order.answers
     answers_total_quantity = 0
     answers.each do |ans|
-      answers_total_quantity =+ ans.quantity
+      answers_total_quantity =+ ans.quantity if ans.quantity.present?
     end
     if answers_total_quantity == 0
       answer.order.quantity
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,17 @@
 module OrdersHelper
+
+  # 納入予定量回答の総量に対しての注文残量[kg]
+  def orders_helper_residual(order)
+    answers = order.answers
+    answers_total_quantity = 0
+    if answers.nil?
+      answers_total_quantity
+    else
+      answers.each do |ans|
+        answers_total_quantity =+ ans.quantity if ans.quantity.present?
+      end
+      order.quantity - answers_total_quantity
+    end
+  end
+
 end
