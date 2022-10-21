b3412fa2543a245f8ab62c62ab47549ffcddbae1
Hiroki Araki <hiro8903@gmail.com>
2022-10-21 20:59:42 +0900

    Inventoryのindexページに前月と次月ボタン追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   
 
   def set_department_monthly_inventory
-    @first_day = Date.current.beginning_of_month
+    @first_day = params[:date].nil? ? Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     @department_monthly_inventory = Inventory.all.where(inventory_at: @first_day..@last_day, department_id: @user.department_id) if @user.department_id.present?
   end
