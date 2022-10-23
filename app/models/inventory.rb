5c28c2b631015ed0e73388a3362cf30a283c4eed
Hiroki Araki <hiro8903@gmail.com>
2022-10-23 16:57:08 +0900

    Inventoryの塗料、部署、年月の重複登録不可を設定

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -7,4 +7,6 @@ class Inventory < ApplicationRecord
     validates :inventory_at
     validates :quantity
   end
+  # 塗料棚卸を登録する際、その塗料、部署、年月の組み合わせが一意であること。
+  validates :paint_id, uniqueness: {scope: [:department_id, :inventory_at]}
 end
