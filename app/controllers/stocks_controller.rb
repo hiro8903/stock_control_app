38d7a6662cebbc947665c2c2c1220dc0fe2c9932
Hiroki Araki <hiro8903@gmail.com>
2023-03-17 12:03:45 +0900

    stockのindexページ作成

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,11 @@
 class StocksController < ApplicationController
+  before_action :set_day, only: [:index]
+
   def index
+    @paints = Paint.all
+    @day = Date.current
+    @last_month = @first_day.prev_month
+    @beginning_stocks = AllInventory.where(inventory_at: @last_month)
   end
 
   def show
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,29 @@
 module StocksHelper
+
+  def beginning_stocks(paint)
+    AllInventory.find_by(inventory_at: @last_month, paint_id: paint.id)
+  end
+
+  def monthly_withdraws_quantity(paint)
+    @monthly_withdraws = Withdraw.where(withdraw_at: @first_day..@day, paint_id: paint.id)
+    @withdraws_quantity = 0
+    @monthly_withdraws.each do |withdraw|
+      @withdraws_quantity += withdraw.quantity
+    end
+    return @withdraws_quantity
+  end
+  
+  def monthly_arrivals_quantity(paint)
+    @monthly_arrivals = Arrival.joins(:order).where(arrival_at: @first_day..@day).where(orders: {paint_id: paint.id})
+
+    @arrivals_quantity = 0.0
+    @monthly_arrivals.each do |arrival|
+      @arrivals_quantity += arrival.quantity
+    end
+    return @arrivals_quantity
+  end
+
+  def stock(paint)
+    beginning_stocks(paint).quantity + @arrivals_quantity - @withdraws_quantity
+  end
 end
